6f87dbe6483c071c8d2faf3faf76b95303aff562
VitaliiOsadchukIF <radistje@gmail.com>
2023-12-13 17:02:09 +0200

    Test1

--- src/main/java/com/example/Dev_20_CI_CD/Dev20CICD.java
+++ src/main/java/com/example/Dev_20_CI_CD/Dev20CICD.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Dev20CICD {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Dev20CICD.class, args);
 	}
